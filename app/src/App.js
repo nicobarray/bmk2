f62bcfb29a57663f76212414b1542fd8db906edc
Nicolas Barray <nbarray@gmail.com>
2018-11-27 23:57:26 +0100

    Add prettier

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 function App() {
- 
-    const [foo, useFoo] = useState(0)
+  const [foo, useFoo] = useState(0)
 
-    return (
-      <div onClick={() => useFoo(foo + 1)}>
-        {foo}
-      </div>
-    );
-
+  return <div onClick={() => useFoo(foo + 1)}>{foo}</div>
 }
 
-export default App;
+export default App
