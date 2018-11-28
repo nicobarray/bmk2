09b88f14f72ebe2559562cca521e706cd70ae038
Nicolas Barray <nbarray@gmail.com>
2018-11-28 01:22:50 +0100

    Add apollo client

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import styled from 'styled-components'
 
@@ -6,13 +8,34 @@ const Layout = styled.div`
   height: 100vh;
 `
 
+const userQuery = gql`
+  query MeQuery($password: String!) {
+    me(password: $password) {
+      name
+    }
+  }
+`
+
 function App() {
   const [foo, useFoo] = useState(0)
 
   return (
-    <Layout>
-      <div onClick={() => useFoo(foo + 1)}>{foo}</div>
-    </Layout>
+    <Query query={userQuery} variables={{ password: 'toto' }}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>
+        if (error) {
+          console.log(error)
+          return <p>Error :(</p>
+        }
+
+        return (
+          <Layout>
+            <div>Hello {data.me.name || 'you'}</div>
+            <div onClick={() => useFoo(foo + 1)}>{foo}</div>
+          </Layout>
+        )
+      }}
+    </Query>
   )
 }
 
