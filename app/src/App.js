48bf1fbceb64a8dd1e5d79cfbd6ddba83d51db13
Nicolas Barray <nbarray@gmail.com>
2018-11-28 01:13:57 +0100

    Add prisma api

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react'
 
+import styled from 'styled-components'
+
+const Layout = styled.div`
+  height: 100vh;
+`
+
 function App() {
   const [foo, useFoo] = useState(0)
 
-  return <div onClick={() => useFoo(foo + 1)}>{foo}</div>
+  return (
+    <Layout>
+      <div onClick={() => useFoo(foo + 1)}>{foo}</div>
+    </Layout>
+  )
 }
 
 export default App
